cd5570a6e222226b040ef93c71044109387e4bce
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-02-22 21:36:05 +0530

    add intersection function

--- utils/index.go
+++ utils/index.go
@@ -1,8 +1,6 @@
 package utils
 
-import (
-	"fmt"
-)
+import "math"
 
 type Index map[string][]int
 
@@ -10,11 +8,31 @@ func (idx Index) Add(docs []document) {
     for _, doc := range docs {
         for _, token := range Analyze(doc.Text) {
             ids := idx[token]
-            fmt.Println(ids)
             if ids != nil && doc.ID == ids[len(ids) - 1] {
                 continue
             }
             idx[token] = append(ids, doc.ID)
         }
     }
+}
+
+func Intersection(a []int, b []int) []int{
+
+    var i, j int
+    maxLen := int(math.Max(float64(len(a)), float64(len(b))))
+    
+    r := make([]int, maxLen)
+
+    for i < len(a) && j < len(b) {
+        if a[i] < b[j] {
+            i++
+        } else if a[i] > b[j] {
+            j++
+        } else {
+            r = append(r, a[i])
+            i++
+            j++
+        }
+    }
+    return r
 }
